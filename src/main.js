0783f4f6213cb921feb23b53cab626e9ade4de5e
pla1n <danilkuisis@gmail.com>
2024-04-11 23:55:56 +0300

    update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
+import "./js/header";
+import "./js/check-year";
+import "./js/guess";
+import "./js/rps";
 import "./js/calculator";
+import "./js/time-calc";
+import "./js/dino";
+import "./js/football";
+import "./js/three-numbers";
+import "./js/team";
+import "./js/scientisc";
+import "./js/footer";
