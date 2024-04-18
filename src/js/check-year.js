97a36f82c8f79add644f4473017542adcb1126d7
pla1n <danilkuisis@gmail.com>
2024-04-19 00:10:23 +0300

    ad

--- src/js/check-year.js
+++ src/js/check-year.js
@@ -1,11 +1,11 @@
-const button = document.querySelector('.search-button');
+const button = document.querySelector('.check-btn-js');
 const input = document.querySelector('.check-input');
 const result = document.querySelector('.check-text');
 
 button.addEventListener('click', showResult);
 
 function showResult() {
-    if(input.value !== '') {
+    if(input.value !== '' && Number(input.value)) {
         if(input.value % 4 === 0) {
             result.textContent = 'Ви народилися у високосний рік!';
             result.style.color = '#00BB00';
--- src/js/time-calc.js
+++ src/js/time-calc.js
@@ -1,4 +1,9 @@
 const input = document.querySelector('.time-input');
-const button = document.querySelector('.search-button');
-const text = document.querySelector('.time-text');
+const button = document.querySelector('.time-btn-js');
+const result = document.querySelector('.time-text');
+
+// const time = {
+//     seconds: input.value / 60,
+//     minutes:
+// }
 
