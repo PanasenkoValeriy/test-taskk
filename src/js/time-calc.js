6a5d7257b9a415159da89a95f81d51c8a57a0134
pla1n <danilkuisis@gmail.com>
2024-04-23 20:13:33 +0300

    update

--- src/js/time-calc.js
+++ src/js/time-calc.js
@@ -2,7 +2,4 @@ const input = document.querySelector('.time-input');
 const button = document.querySelector('.time-btn-js');
 const result = document.querySelector('.time-text');
 
-// const time = {
-//     seconds: input.value / 60,
-//     minutes:
-// }
+result.textContent = 'asdasdasd'
