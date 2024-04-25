b43aa06af35b0d51f58b14abaf12aa1ff624ee81
pla1n <danilkuisis@gmail.com>
2024-04-25 20:09:18 +0300

    update

--- src/js/time-calc.js
+++ src/js/time-calc.js
@@ -1,17 +1,41 @@
 const input = document.querySelector('.time-input');
 const button = document.querySelector('.time-btn-js');
-let days = document.getElementById('days');
-let hours = document.getElementById('hours');
-let minutes = document.getElementById('minutes');
-let seconds = document.getElementById('seconds');
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const result = document.querySelector('.time-text');
+days.style.marginRight = '5px';
+
 
 button.onclick = function converter() {
-    days = Math.floor(input.value / (24 * 60));
-    hours = Math.floor((input.value - (days * (24 * 60))) / 60);
-    minutes = input.value - ((days * (24 * 60)) + hours * 60);
+    const daysConvertor = Math.floor(input.value / (24 * 60));
+    const hoursConvertor = Math.floor((input.value - daysConvertor * 24 * 60) / 60);
+    const minutesConvertor = Math.floor(input.value - (daysConvertor * 24 * 60 + hoursConvertor * 60));
+    const secondsConvertor = Math.floor((input.value - (daysConvertor * 24 * 60 + hoursConvertor * 60 + minutesConvertor)) * 60);
+
+    days.textContent = `${daysConvertor} дн.`;
+
+    if (hoursConvertor >= 10) {
+        hours.textContent = `${hoursConvertor}:`;
+    } else {
+        hours.textContent = `0${hoursConvertor}:`;
+    }
+
+    if (minutesConvertor >= 10) {
+        minutes.textContent = `${minutesConvertor}:`;
+    } else {
+        minutes.textContent = `0${minutesConvertor}:`;
+    }
+
+    if (secondsConvertor >= 10) {
+        seconds.textContent = `${secondsConvertor}`;
+    } else {
+        seconds.textContent = `0${secondsConvertor}:`;
+    }
+    
     
 
-    console.log(days);
-    console.log(hours);
-    console.log(minutes);
+    // result.textContent = `${daysConvertor} дн. ${hoursConvertor}:${minutesConvertor}:${secondsConvertor}`
+
 }
