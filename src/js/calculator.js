22ffe06b0e003ceccffd509e516871e56c0ee789
pla1n <danilkuisis@gmail.com>
2024-04-25 18:38:22 +0300

    update

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,5 +1,4 @@
 const calculatorButton = document.querySelector('.calculator-button');
-console.log(calculatorButton)
 calculatorButton.addEventListener('click', () => {
     calculatorButton.classList.toggle('active-calc-btn')
 })
