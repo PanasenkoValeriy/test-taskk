c5350e6c0b6ea9082d857f20fc9686453430c15e
pla1n <danilkuisis@gmail.com>
2024-04-30 17:08:18 +0300

    update

--- src/js/scientisc.js
+++ src/js/scientisc.js
@@ -88,3 +88,10 @@ const scientists = [
 
 const card = document.querySelectorAll('.scientist-card');
 const button = document.querySelector('.scientist-option-button');
+const text = document.querySelectorAll('.scientist-card-text');
+// const list = document.querySelectorAll('.scientist-card-list');
+
+button.onclick = function century() {
+  scientists.filter(scientist => scientist.born >= 1800 && scientist.born < 1900)
+  
+}
