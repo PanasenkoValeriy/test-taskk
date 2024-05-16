b13b4186fe20ca35ea8633d11a122eaca8b4dcb6
pla1n <danilkuisis@gmail.com>
2024-05-16 17:30:53 +0300

    update

--- src/js/team.js
+++ src/js/team.js
@@ -1,3 +1,8 @@
+import aleksImgUrl from '../img/team-imgs/Aleks.jpg';
+import romaImgUrl from '../img/team-imgs/Roma.jpg';
+import danyaImgUrl from '../img/team-imgs/Danya.jpg';
+import valeraImgUrl from '../img/team-imgs/Valera.jpg';
+
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
 
@@ -13,3 +18,15 @@ const swiper = new Swiper('.swiper', {
   speed: 800,
   keyboard: true,
 });
+
+const aleksImg = document.querySelector('.aleks');
+const romaImg = document.querySelector('.roma');
+const danyaImg = document.querySelector('.danya');
+const valeraImg = document.querySelector('.valera');
+
+const imgs = [aleksImg, romaImg, danyaImg, valeraImg];
+const imgUrls = [aleksImgUrl, romaImgUrl, danyaImgUrl, valeraImgUrl];
+
+for(let i = 0; i < imgs.length; i++) {
+  imgs[i].src += imgUrls[i];
+}
